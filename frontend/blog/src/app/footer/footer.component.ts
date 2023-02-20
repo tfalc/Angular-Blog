0be3e74f54b016c264f8015fee7759165338516d
Thiago F <thiago.falcao86@gmail.com>
2023-02-19 22:33:21 -0300

    feat: update footer code

--- frontend/blog/src/app/footer/footer.component.ts
+++ frontend/blog/src/app/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 @Component({
   selector: 'app-footer',
@@ -6,5 +10,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+
+  faInstagram = faInstagram
+  faFacebook = faFacebook
+  faLinkedIn = faLinkedin
 
 }
